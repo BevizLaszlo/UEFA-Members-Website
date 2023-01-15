648d4b2ff86f6a03768bde77e3a266f854fdb3cb
BevizLaszlo <bevizlaci@gmail.com>
2023-01-15 19:15:09 +0100

    json update

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// imported data from the json files
+import languages from "./json/languages.json" assert {type: 'json'};
+import nations from "./json/nations.json" assert {type: 'json'}; 
+
+
 /**
  * this function:
  *  - disable scrolling down with the 'space' or 'down' key in the website when the pop up panel is opened
@@ -19,132 +24,13 @@ document.onkeydown = (event) => {
     }
 }
 
-
-/*
- * Languages:
- * 0: hungarian
- * 1: english
- * 2: german
- */
-
-const languages = [
-    {
-        title: "UEFA tagországok",
-        smallTitle: "tagországok",
-        moreInfo: "több info",
-        homeKit: "Hazai mez",
-        awayKit: "Idegenbeli mez",
-        thirdKit: "Harmadik mez",
-        goBack: "vissza",
-        competitions: "UEFA versenyszámok",
-        national: "Válogatottak",
-        clubs: "Klubcsapatok",
-        more: "Több"
-    },
-    {
-        title: "UEFA Members",
-        smallTitle: "Members",
-        moreInfo: "more info",
-        homeKit: "Home Kit",
-        awayKit: "Away Kit",
-        thirdKit: "Third Kit",
-        goBack: "Go back",
-        competitions: "UEFA Competitions",
-        national: "For national teams",
-        clubs: "For clubs",
-        more: "More"
-    },
-    {
-        title: "UEFA-Mitgliedsverbände",
-        smallTitle: "Mitgliedsverbände",
-        moreInfo: "mehr info",
-        homeKit: "Heimtrikot",
-        awayKit: "Auswärtstrikot",
-        thirdKit: "Drittes Trikot",
-        goBack: "Zurück",
-        competitions: "Wettbewerbe",
-        national: "Für Nationalmannschaften",
-        clubs: "Für Vereinsmannschaften",
-        more: "Mehr"
-    }
-]
-
-const Nations = {
-    /*
-        0: magyar név / hungarian name
-        1: angol név / english name
-        2: német név / german name
-    */     
-    "alb": ["Albánia", "Albania", "Albanien"],
-    "and": ["Andorra", "Andorra", "Andorra"],
-    "arm": ["Örményország", "Armenia", "Armenien"],
-    "aut": ["Ausztria", "Austria", "Österreich"],
-    "aze": ["Azerbajdzsán", "Azerbaijan", "Aserbaidschan"],
-    "bel": ["Belgium", "Belgium", "Belgien"],
-    "bih": ["Bosznia-Hercegovina", "Bosnia and Herzegovina", "Bosnien und Herzegowina"],
-    "blr": ["Fehéroroszország", "Belarus", "Belarus"],
-    "bul": ["Bulgária", "Bulgaria", "Bulgarien"],
-    "cro": ["Horvátország", "Croatia", "Kroatien"],
-    "cyp": ["Ciprus", "Cyprus", "Zypern"],
-    "cze": ["Csehország", "Czech Republic", "Tschechien"],
-    "den": ["Dánia", "Denmark", "Dänemark"],
-    "eng": ["Anglia", "England", "England"],
-    "esp": ["Spanyolország", "Spain", "Spanien"],
-    "est": ["Észtország", "Estonia", "Estland"],
-    "fin": ["Finnország", "Finland", "Finnland"],
-    "fra": ["Franciaország", "France", "Frankreich"],
-    "fro": ["Feröer-szigetek", "Faroe Islands", "Färöer"],
-    "geo": ["Grúzia", "Georgia", "Georgien"],
-    "ger": ["Németország", "Germany", "Deutschland"],
-    "gib": ["Gibraltár", "Gibraltar", "Gibraltar"],
-    "gre": ["Görögország", "Greece", "Griechenland"],
-    "hun": ["Magyarország", "Hungary", "Ungarn"],
-    "irl": ["Írország", "Republic of Ireland", "Irland"],
-    "isl": ["Izland", "Iceland", "Island"],
-    "isr": ["Izrael", "Israel", "Israel"],
-    "ita": ["Olaszország", "Italy", "Italien"],
-    "kaz": ["Kazahsztán", "Kazakhstan", "Kasachstan"],
-    "kvx": ["Koszovó", "Kosovo", "Kosovo"],
-    "lie": ["Liechtenstein", "Liechtenstein", "Liechtenstein"],
-    "ltu": ["Litvánia", "Lithuania", "Litauen"],
-    "lux": ["Luxemburg", "Luxembourg", "Luxemburg"],
-    "lva": ["Lettország", "Latvia", "Lettland"],
-    "mda": ["Moldova", "Moldova", "Moldau"],
-    "mkd": ["Észak-Macedónia", "North Macedonia", "Nordmazedonien"],
-    "mlt": ["Málta", "Malta", "Malta"],
-    "mne": ["Montenegró", "Montenegro", "Montenegro"],
-    "ned": ["Hollandia", "The Netherlands", "Niederlande"],
-    "nir": ["Észak-Írország", "Northern Ireland", "Nordirland"],
-    "nor": ["Norvégia", "Norway", "Norwegen"],
-    "pol": ["Lengyelország", "Poland", "Polen"],
-    "por": ["Portugália", "Portugal", "Portugal"],
-    "rou": ["Románia", "Romania", "Rumänien"],
-    "rus": ["Oroszország", "Russia", "Russland"],
-    "sco": ["Skócia", "Scotland", "Schottland"],
-    "smr": ["San Marino", "San Marino", "San Marino"],
-    "srb": ["Szerbia", "Serbia", "Serbien"],
-    "sui": ["Svájc", "Switzerland", "Schweiz"],
-    "svk": ["Szlovákia", "Slovakia", "Slowakei"],
-    "svn": ["Szlovénia", "Slovenia", "Slowenien"],
-    "swe": ["Svédország", "Sweden", "Schweden"],
-    "tur": ["Törökország", "Türkiye", "Türkei"],
-    "ukr": ["Ukrajna", "Ukraine", "Ukraine"],
-    "wal": ["Wales", "Wales", "Wales"],
-}
-
-
+// rendering languages
 let lang = 1;
-document.querySelector(".hungarian").onclick = () => {
-    lang = 0;
-    renderLanguage();
-}
-document.querySelector(".english").onclick = () => {
-    lang = 1;
-    renderLanguage();
-}
-document.querySelector(".german").onclick = () => {
-    lang = 2;
-    renderLanguage();
+for (const language of languages) {
+    document.querySelector(`.${language.lang}`).onclick = () => {
+        lang = language.id;
+        renderLanguage();
+    }
 }
 
 function renderLanguage() {
@@ -184,14 +70,12 @@ function removeElement(element1, element2) {
 }
 
 
-
-
-for (let id in Nations){
+for (let id in nations){
     // country hover titles:
     document.getElementById(id).onmousemove = () => {
         document.querySelector(".country-title").innerHTML = `
-            <img src="./img/flags/${id}.png" alt="${Nations[id][lang]}">
-            <h3>${Nations[id][lang]}</h3>
+            <img src="./img/flags/${id}.png" alt="${nations[id][lang]}">
+            <h3>${nations[id][lang]}</h3>
             `;
         document.querySelector(".country-title").classList.remove("hide");
         let boxWidth = document.querySelector(".country-title").offsetWidth;
@@ -214,7 +98,7 @@ for (let id in Nations){
     // country pop ups:
     document.getElementById(id).onclick = () => {
         document.getElementById('favicon').setAttribute('href',`./img/flags/${id}.png`);
-        document.title = Nations[id][lang];
+        document.title = nations[id][lang];
         document.getElementById("description").innerHTML = `
         <div id="overlay"></div>
         
@@ -222,17 +106,17 @@ for (let id in Nations){
                 <header>
                     <div></div>
                     <div id="FlagName">
-                        <img src="./img/flags/${id}.png" alt="${Nations[id][lang]} flag">
-                        <h2>${Nations[id][lang]}</h2>
+                        <img src="./img/flags/${id}.png" alt="${nations[id][lang]} flag">
+                        <h2>${nations[id][lang]}</h2>
                     </div>
                     <button class="closeBtn" onClick="closeDescription()">&times;</button>
                 </header>
                 <main>
                     <table>
                         <tr>
-                            <td><img src="./img/kits/${id}1.png" alt="${Nations[id][lang]} home kit"></td>
-                            <td><img src="./img/kits/${id}2.png" alt="${Nations[id][lang]} away kit"></td>
-                            <td id="kit3"><img src="./img/kits/${id}3.png" alt="${Nations[id][lang]} third kit" onError="removeElement(kit3, kit3des)"></td>
+                            <td><img src="./img/kits/${id}1.png" alt="${nations[id][lang]} home kit"></td>
+                            <td><img src="./img/kits/${id}2.png" alt="${nations[id][lang]} away kit"></td>
+                            <td id="kit3"><img src="./img/kits/${id}3.png" alt="${nations[id][lang]} third kit" onError="removeElement(kit3, kit3des)"></td>
                         </tr>
                         <tr id="secondRow">
                             <td>${languages[lang].homeKit}</td>
